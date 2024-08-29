97138a6f41775c27b1d5b98893610ca066bc1a77
Sim <64483242+SimBienvenueHoulBoumi@users.noreply.github.com>
2024-08-29 10:48:26 +0200

    update auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,7 +9,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthDto } from './dto/auth.dto';
 import { AuthGuard } from './auth.guard';
 
@@ -18,12 +18,14 @@ import { AuthGuard } from './auth.guard';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Sign up' })
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: AuthDto) {
     return this.authService.signIn(signInDto);
   }
 
+  @ApiOperation({ summary: 'Sign in' })
   @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
